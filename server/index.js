df9b112a58a4d2c5435169c45819a6efb549c27c
harshit-01 <kharshit0101@gmail.com>
2022-12-05 01:25:39 +0530

    server cors issue change

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const app = express();
 
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/posts',postRouter);
 app.get('/',(req, res) =>{
      res.send("Hello to Memories API");
